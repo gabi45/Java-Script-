737bc1a5454bcace841c1ada7eb176eef762da82
Gabriele Francine Santos Alves <43455767+gabi45@users.noreply.github.com>
2023-11-18 17:48:07 -0300

    Add files via upload

---
+++ Script01.js
@@ -0,0 +1,30 @@
+//trabalhar com variáveis,console.log
+
+//Utilizando padrão camelcase
+
+//variável (let,var,const) -> identificador ->dados 
+
+const Carro = "Carro"
+const fabricanteCarro= "renault"
+const modelocarro = "sandero"
+const corcarro = "vermelha"
+const anoCarro = 2010
+//primweira forma: concatenação
+
+// saida de dados 
+// carro: "carro"
+// fabricantecarro: "renault"
+// modelocarro: "sandero"
+//corcarro: "cor"
+//anocarro: anocarro
+
+console ("carro"+ carro + "fabricante" + fabricante:)"  
+fabricantecarro + ", modelo do carro:" + modelocarro +", cor do carro:" + corcarro + ", ano:" + anocarro);
+console.log ("carro");
+console.log ("fabricante do carro");
+console.log ("modelocarro");
+console.log ("corcarro")
+console.log ("anoCarro");
+
+// Segunda forma:Interpolação
+console.log  (`Meu carro ${Carro}, fabricante  do carro: ${fabricanteCarro},  modelo carro: ${modelocarro}, COR DO CARRO: ${corcarro}, ano do carro: ${anoCarro}`);
